a49ed613a000584fcf203b8fb0d298d0b27063bf
Petro Khovrashchuk <petro.khovrashchuk@gmail.com>
2026-04-08 13:56:43 +0200

    add initial files

---
+++ src/main/java/com/fidelix/coding_club_a/schema_morph/SchemaMorphApplication.java
@@ -0,0 +1,13 @@
+package com.fidelix.coding_club_a.schema_morph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaMorphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaMorphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fidelix/coding_club_a/schema_morph/SchemaMorphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidelix.coding_club_a.schema_morph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaMorphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

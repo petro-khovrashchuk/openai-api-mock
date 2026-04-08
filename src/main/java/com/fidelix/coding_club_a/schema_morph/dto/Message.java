a442d90591a138ccf64adf6afeba17a2d6d0cfd0
Petro Khovrashchuk <petro.khovrashchuk@gmail.com>
2026-04-08 20:16:34 +0200

    remove old folder

--- src/main/java/com/fidelix/coding_club_a/schema_morph/dto/Message.java
+++
@@ -1,5 +0,0 @@
-package com.fidelix.coding_club_a.schema_morph.dto;
-
-public record Message(String role, String content) {
-
-}
--- src/main/java/com/fidelix/coding_club_a/schema_morph/service/MockDataGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.fidelix.coding_club_a.schema_morph.service;
-
-import tools.jackson.databind.JsonNode;
-
-public interface MockDataGenerator {
-
-  JsonNode generateData(JsonNode schema);
-}
